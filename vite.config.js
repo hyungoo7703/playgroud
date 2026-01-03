99ebf6c4eda353a2c856d0f168e1b3f11463f524
Hyungoo Kang <hyungoo7703@gmail.com>
2026-01-04 00:19:30 +0900

    저장소 이름으로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/family-playground/' // GitHub Pages 배포를 위해 레포지토리 이름으로 변경해주세요.
-})
+  base: "/playground/", // GitHub Pages 배포를 위해 레포지토리 이름으로 변경해주세요.
+});
