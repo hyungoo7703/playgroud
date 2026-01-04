4743d47c3fca398426933c3f35fd0e0a90065397
Hyungoo Kang <hyungoo7703@gmail.com>
2026-01-04 17:45:13 +0900

    다크모드 대응

--- src/routes/BulletinBoard.svelte
+++ src/routes/BulletinBoard.svelte
@@ -1,4 +1,4 @@
 <div class="p-4">
-  <h2 class="text-2xl font-bold mb-4">게시판</h2>
-  <p>게시판은 현재 준비 중입니다. 곧 찾아뵙겠습니다!</p>
+  <h2 class="text-2xl font-bold mb-4 dark:text-white">게시판</h2>
+  <p class="dark:text-white">게시판은 현재 준비 중입니다. 곧 찾아뵙겠습니다!</p>
 </div>
